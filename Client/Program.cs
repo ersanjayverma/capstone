e7dafb9f44225ce55b44a4c5102bc00bdbc9f690
Sanjay Verma <sanjay@blackhatbadshah.com>
2025-07-10 07:10:41 +0200

    Theme midway

--- Client/Program.cs
+++ Client/Program.cs
@@ -23,10 +23,10 @@ builder.Services.AddOidcAuthentication(options =>
     options.ProviderOptions.Authority = "https://auth.blackhatbadshah.com/realms/ztacs";
     options.ProviderOptions.ClientId = "25pSHCWTZQHPupUPAXimVmvcIDKaUIEK";
     options.ProviderOptions.ResponseType = "code";
-    options.ProviderOptions.RedirectUri = "https://capstone.blackhatbadshah.com";
+    options.ProviderOptions.RedirectUri = "https://capstone.blackhatbadshah.com/welcome";
 
     // Optional but recommended
-    options.ProviderOptions.PostLogoutRedirectUri = "https://capstone.blackhatbadshah.com";
+    options.ProviderOptions.PostLogoutRedirectUri = "https://capstone.blackhatbadshah.com/bye";
     options.ProviderOptions.DefaultScopes.Add("openid");
     options.ProviderOptions.DefaultScopes.Add("profile");
     options.ProviderOptions.DefaultScopes.Add("email");
