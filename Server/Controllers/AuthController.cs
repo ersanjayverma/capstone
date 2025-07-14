ab6257974bafa822d14c2f16120e8748578e8926
Sanjay.Verma.Programming.com <Blackhatbadshah@outlook.com>
2025-07-14 02:22:44 -1200

    Added AuthService

---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using ZTACS.Shared.Models;
+
+namespace ZTACS.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public AuthController(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        [HttpPost("get-token")]
+        public async Task<IActionResult> GetToken([FromBody] KeycloakTokenRequest request)
+        {
+            var tokenEndpoint = $"{_config["Auth0:Domain"]}/realms/ztacs/protocol/openid-connect/token";
+
+            var parameters = new Dictionary<string, string>
+            {
+                { "grant_type", "password" },
+                { "client_id", _config["Keycloak:ClientId"] },
+                { "client_secret", _config["Keycloak:ClientSecret"] },
+                { "username", request.Username },
+                { "password", request.Password }
+            };
+
+            var content = new FormUrlEncodedContent(parameters);
+
+            var response = await _httpClient.PostAsync(tokenEndpoint, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                return BadRequest(error);
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return Ok(JsonDocument.Parse(responseString));
+        }
+    }
+
+}
