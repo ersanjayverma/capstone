f4e758839e97c5f7682e09525328f3772c3c8f01
Data Server <dataserver@vmi1820680.contaboserver.net>
2025-10-02 17:09:09 +0200

    Swagger Data Added - bug fix

--- src/main/java/com/capstone/Arogya/dto/AiResponse.java
+++ src/main/java/com/capstone/Arogya/dto/AiResponse.java
@@ -1,9 +1,7 @@
 package com.capstone.Arogya.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
